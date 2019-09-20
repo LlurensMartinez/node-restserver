8833ed860c3bfe9e16dfe6c4d5e7f6cdf02860db
Llorenç <crack.one.7@gmail.com>
2019-09-20 09:25:23 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/elsgallsweb'
 }else {
-  urlDB = 'mongodb+srv://llorens:aoTBbp0zd4DgNSeq@cluster0-2qh5y.mongodb.net/ElsGallsWeb'
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
